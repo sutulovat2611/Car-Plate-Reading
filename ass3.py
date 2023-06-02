76c4af0c1329f23c2fff77a25467cc1505b6335a
Tatiana Sutulova <tsut0005@student.monash.edu>
2023-06-02 18:56:59 +0800

    combining with auto segment

--- ass3.py
+++ ass3.py
@@ -6,6 +6,9 @@ import random
 import math
 
 
+import functools
+
+
 def Weight_Initialization():
     # Initializing of the Weights. Random float number between -0.5 to 0.5 for weights.
     np.random.seed(1)
@@ -83,6 +86,98 @@ def Weight_Bias_Update(wkj,dwkkj, bias_k, dbkkj, wji, dwjji,bias_j,dbjii ):
     # Save 𝑤𝑗𝑗𝑖 and 𝑏𝑗𝑗𝑖
 
 
+
+
+def auto_segmentation():
+    target_fd = "./target"
+    file_list = os.listdir(target_fd)
+
+    counter = 0
+    for img in file_list:
+        naming = img[:-4]
+
+        # Read the image and convert to grayscale
+        image = cv2.imread("./target/" + img)
+        h, w, *_ = image.shape
+
+        # Resizing images  
+        if ( w < 300):
+            scale_percent = 320 # percent of original size
+        elif ( w < 800):
+            scale_percent = 220 # percent of original size
+        else:
+            scale_percent = 50 # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+        # copying the image to use later
+        result = image.copy()
+
+        # Converting to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Apply Gaussian blurring and thresholding to reveal the characters on the license plate
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 101, 1)
+
+        # Perform connected components analysis on the thresholded image and initialize the mask to hold only the components we are interested in
+        _, labels = cv2.connectedComponents(thresh)
+        mask = np.zeros(thresh.shape, dtype="uint8")
+
+        # Loop over the unique components
+        for (i, label) in enumerate(np.unique(labels)):
+            # If this is the background label, ignore it
+            if label == 0:
+                continue
+            # Otherwise, constrsuct the label mask to display only connected component for the current label
+            labelMask = np.zeros(thresh.shape, dtype="uint8")
+            labelMask[labels == label] = 255
+            numPixels = cv2.countNonZero(labelMask)
+        
+            # Add to our mask
+            mask = cv2.add(mask, labelMask)
+
+        # Find contours and get bounding box for each contour
+        cnts, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+
+        # Sort the bounding boxes from left to right
+        def compare(rect1, rect2): 
+            return rect1[0] - rect2[0]
+        boundingBoxes = sorted(boundingBoxes, key=functools.cmp_to_key(compare) )
+        char = 0
+        # Get contours
+        for bnd in boundingBoxes:
+            x,y,w,h = bnd
+            if (h>0.5*image.shape[0] and h<0.95*image.shape[0]):
+                # Crops out the detected area from the original image
+                crop = result[y-7:y+h+10, x-7:x+w+10]       
+                crop_def = result[y:y+h, x:x+w]       
+
+                # Crops out the detected area from the original image & saves the cropped image
+                try:
+                    cv2.imwrite('results/'+str(counter)+"_"+str(char)+"_"+naming[char]+'.jpg', crop)
+                except:
+                    cv2.imwrite('results/'+str(counter)+"_"+str(char)+"_"+naming[char]+'.jpg', crop_def)
+                char+=1
+                
+        counter+=1
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     OUTPUT_NEURONS = 20
     INPUT_NEURONS = 28* 28
@@ -145,9 +240,51 @@ if __name__ == "__main__":
     accuracy = 0
 
     #test
-    target_fd = "./character_image/test_case"
+    # target_fd = "./character_image/test_case"
+    # for name in listdir(target_fd):
+    #     label = name[0]
+    #     try:
+    #         label_value = int(label)
+    #     except ValueError:
+    #         label_value = alphabets_targets.get(label)
+    #         assert label_value is not None, "label_value is None"
+
+
+    #     image =cv2.imread(os.path.join(target_fd,name))
+    #     resized = cv2.resize(image, (28,28))
+    #     # convert picture to gray scale
+    #     img_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    #     _,img = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY)
+    #     x_flattend = img.reshape(1, 28*28)
+
+    #     x_flattend = np.squeeze(x_flattend)
+    #     x_flattend = x_flattend/255
+
+    #     mean = np.mean(x_flattend)
+    #     std = np.std(x_flattend)
+    #     x_normalized = (x_flattend - mean) / std
+
+    #     inputs  = x_normalized
+
+    #     # inputs  = x_flattend
+    #     netj,outj = Forward_Input_Hidden(inputs, wji, bias_j)
+    #     netk,outk = Forward_Hidden_Output(outj, wkj, bias_k)
+    #     # print(np.argmax(outk))
+    #     # print(outk)
+    #     if np.argmax(outk) == label_value :
+    #         accuracy +=1
+
+    # print("accuracy")
+    # print(accuracy)
+
+
+
+    # Working with segmentation
+    # auto_segmentation()
+
+    target_fd = "./results"
     for name in listdir(target_fd):
-        label = name[0]
+        label = name[4]
         try:
             label_value = int(label)
         except ValueError:
@@ -181,5 +318,9 @@ if __name__ == "__main__":
 
     print("accuracy")
     print(accuracy)
+
+
+
+
 
 
